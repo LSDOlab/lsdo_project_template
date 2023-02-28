69939a302a152b05100a0d9957511ab37535638e
anugrahjo <44123535+anugrahjo@users.noreply.github.com>
2023-02-28 11:02:57 -0800

    added setup.py for direct installation from terminal with all dependencies

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lsdo_project_template',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+#         'scipy',
+#         'pint',
+#         'sphinx-rtd-theme',
+#         'sphinx-code-include',
+#         'jupyter-sphinx',
+#         'numpydoc',
+    ],
+)
